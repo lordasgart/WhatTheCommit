f93b6b7dbf76b3faed410351783531921ed9e23e
Markus Malz <markus.malz@outlook.com>
2021-04-01 15:04:26 +0200

    I am Root. We are Root.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Flurl.Http;
+
+namespace WhatTheCommit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var result = await "http://whatthecommit.com".GetAsync().ReceiveString();
+
+            Console.WriteLine(result);
+
+            XmlDocument htmlResponse = new XmlDocument();
+            htmlResponse.LoadXml(result.Replace("<!doctype html>",string.Empty).Replace("<meta charset=\"UTF-8\">", string.Empty));
+
+            var commitMessage = htmlResponse.SelectSingleNode("/html/body/div/p").InnerText.Trim();
+
+            Console.WriteLine(commitMessage);
+        }
+    }
+}
